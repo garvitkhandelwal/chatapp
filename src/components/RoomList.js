2f8f0af6ca10a27ead75231d7bd33969236456db
Garvit Khandelwal <garvitkhandelwal14@gmail.com>
2019-03-13 02:50:30 +0530

    added autoscroll

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 class RoomList extends Component {
 	render() {
+		const orderedRooms = [...this.props.rooms].sort((a,b) => a.id - b.id)
 		return(
-			<div className="rooms-List">
+			<div className="rooms-list">
 				<div className="help-text">
 					<ul>
 					<h3>Your Rooms:</h3>
-					{this.props.rooms.map(room => {
+					{orderedRooms.map(room => {
+						const active = this.props.roomId === room.id ? "active" : "";
 						return(
-							<li key={room.id} className="room">
+							<li key={room.id} className={"room " + active}>
 								<a
 									onClick = {() => this.props.subscribeToRoom(room.id)}
 									href="#">
