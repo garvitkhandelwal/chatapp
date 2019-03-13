bae1036758fa8607e48dfe8da7374ea7ea9512e9
Garvit Khandelwal <garvitkhandelwal14@gmail.com>
2019-03-14 03:23:10 +0530

    added create room feature

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
 		this.sendMessage = this.sendMessage.bind(this)
 		this.subscribeToRoom = this.subscribeToRoom.bind(this)
 		this.getRooms = this.getRooms.bind(this)
+		this.createRoom = this.createRoom.bind(this)
 	}
 
 	componentDidMount() {
@@ -81,6 +82,14 @@ class App extends Component {
 		})
 	}
 
+	createRoom(name) {
+		this.currentUser.createRoom({
+			name
+		})
+		.then(room => this.subscribeToRoom(room.id))
+		.catch(err => console.log('Error in creating room: ', err))
+	}
+
   render() {
     return (
       <div className="App">
@@ -88,9 +97,13 @@ class App extends Component {
 					roomId = {this.state.roomId}
 					subscribeToRoom= {this.subscribeToRoom}
 					rooms= {[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
-				<MessageList messages= {this.state.messages} />
-				<SendMessageForm sendMessage= {this.sendMessage}/>
-				<NewRoomForm />
+				<MessageList
+					roomId={this.state.roomId}
+					messages= {this.state.messages} />
+				<SendMessageForm
+					disabled={!this.state.roomId}
+					sendMessage= {this.sendMessage}/>
+				<NewRoomForm createRoom = {this.createRoom} />
       </div>
     );
   }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,33 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import Message from './Message'
 
-class MessageList extends Component {
+class MessageList extends React.Component {
 
-	componentWillUpdate() {
-		const node = ReactDOM.findDOMNode(this)
-		this.shouldScrollToBottom = node.scrollTop + node.clientHeight +100 >= node.scrollHeight
-	}
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
 
-	componentDidUpdate() {
-		if(this.shouldScrollToBottom){
-			const node = ReactDOM.findDOMNode(this)
-			node.scrollTop = node.scrollHeight
-		}
-	}
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight
+        }
+    }
 
-
-	render() {
-		return (
-			<div className="message-list">
-				{this.props.messages.map((message, index) => {
-					return (
-						<Message key={index} username={message.senderId} text={message.text}/>
-					);
-				})}
-			</div>
-		);
-	}
+    render() {
+        if(!this.props.roomId) {
+          return (
+            <div className="message-list">
+              <div className="join-room">
+                &larr; Join a room!
+              </div>
+            </div>
+          )
+        }
+        return (
+            <div className="message-list">
+                {this.props.messages.map((message, index) => {
+                    return (
+                        <Message key={message.id} username={message.senderId} text={message.text} />
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
-export default MessageList;
+export default MessageList
